766a9ecacc034b13a02871a6ef65ab1b433362ec
ZsoltLerant89 <lerant.zsolt89@gmail.com>
2024-05-19 13:15:47 +0200

    classes, db Connection

--- sb_carrent_mvc/src/main/java/pti/sb_carrent_mvc/controller/AppController.java
+++ sb_carrent_mvc/src/main/java/pti/sb_carrent_mvc/controller/AppController.java
@@ -1,5 +1,26 @@
 package pti.sb_carrent_mvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import pti.sb_carrent_mvc.service.AppService;
+
+@Controller
 public class AppController {
 
+	private AppService service;
+
+	@Autowired
+	public AppController(AppService service) {
+		super();
+		this.service = service;
+	}
+	
+	@GetMapping("/")
+	private String indexPage()
+	{
+		return "index.html";
+	}
+	
 }
--- sb_carrent_mvc/src/main/java/pti/sb_carrent_mvc/service/AppService.java
+++ sb_carrent_mvc/src/main/java/pti/sb_carrent_mvc/service/AppService.java
@@ -1,5 +1,20 @@
 package pti.sb_carrent_mvc.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pti.sb_carrent_mvc.db.Database;
+
+@Service
 public class AppService {
 
+	private Database db;
+
+	@Autowired
+	public AppService(Database db) {
+		super();
+		this.db = db;
+	}
+	
+	
 }
